2365cdca67cf456efed241d3f2ba648cad4b92e2
jkearney <jkearney486@gmail.com>
2018-04-11 17:10:20 -0400

    I have no idea what I'm doing

---
+++ GranblueGrindTracker/GranblueGrindTracker/ClientApp/components/ItemCard.tsx
@@ -0,0 +1,24 @@
+﻿import * as React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+
+type ItemProps = RouteComponentProps<{
+    name: string;
+    amountHeld: number;
+    amountRequired: number;
+}>;
+
+class Item extends React.Component<ItemProps, {}> {
+    public render() {
+        return <div>
+            <div>
+                {this.props.match.params.name}
+            </div>
+            <div>
+                {this.props.match.params.amountHeld}
+                /
+                {this.props.match.params.amountRequired}
+            </div>
+        </div>;
+    }
+}
+
